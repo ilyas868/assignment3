789ed06a8044e4333be8905aea838efc71dc991d
ilyas868 <ilyas.rahim86@gmail.com>
2022-03-12 01:35:58 +0500

    done

---
+++ main.dart
@@ -0,0 +1,141 @@
+// Assigment 3
+
+// Question no1
+
+  //abhi ni kiya
+
+
+//Question 2
+
+// void main() {
+//   var ticket = 600;
+//   var total = ticket * 6;
+//   print(total);
+// }
+
+// ----------------------------------
+
+//Question 3
+
+// void main() {
+//   List list1 = [1, 2, 3, 4, 5, 6, 7];
+//   List list2 = [3, 5, 6, 7, 9, 10];
+//   print(list1.take(4)) ;
+//
+// }
+//------------------------------
+
+// Question 4
+
+// ??=
+// Assign the value only if the variable is null
+
+// ?=
+
+// null value
+
+//----------------------------------------------------
+
+//Question no 5
+
+// 1) integer
+// 2) variable
+// 3) string
+// 4) bool
+
+// integer{
+
+//   int no1 = 2;
+//   int no2 =3;
+//   print(no1+no2);
+// }
+
+// variable{
+
+//   var ass = 32;
+//   var ass1 = ass+11;
+//   print(ass1);
+
+// }
+
+// String{
+
+//  var np = "ilyas";
+//  print(np);
+
+// }
+
+// bool{
+
+// var num1 = 12;
+// var num2 = 43;
+// Bool aa= num1 < num2{
+//   print(aa);
+// }
+
+// }
+
+//-------------------------------------------
+
+//Quesion no 6
+
+// void main() {
+//   var arr1 = [7, 14, 21, 28, 35, 42, 49, 56, 63, 70];
+//   var arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//   print(arr1.map((e) => arr1));
+// }
+
+//------------------------------------------------------------
+
+//Question 7
+
+// import 'dart:io';
+
+// void main() {
+//   var passwod = "admin123";
+//   print("Please enter your password");
+//   var check = stdin.readLineSync();
+
+//   if (passwod == check) {
+//     print("Correct!");
+//   } else {
+//     print("incorrect");
+//   }
+// }
+
+//-------------------------------------------
+
+//Question no 8
+
+// void main() {
+//   var arr1 = ["hamza", "talha", "ali"];
+//   var arr2 = [456, 399, 276];
+//   var totalMarks = 500;
+//   var prcntge0 = arr2[0] / totalMarks * 100;
+//   var prcntge1 = arr2[1] / totalMarks * 100;
+//   var prcntge2 = arr2[2] / totalMarks * 100;
+//   print("$arr1[0], $arr2[0], $prcntge0");
+//     print("$arr1[1], $arr2[1], $prcntge1");
+//       print("$arr1[2], $arr2[2], $prcntge2");
+
+// }
+
+//-----------------------------------------------------
+
+//question 10
+/
+// void main() {
+//   var arr = ["Hyderabad"];
+//   var paste = "islam";
+//   var newvalue = arr.indexOf(paste);
+//   arr.replaceRange(0, 5 , [paste]);
+//   print(arr);
+// }
+
+
+
+//question 10
+
+
+
